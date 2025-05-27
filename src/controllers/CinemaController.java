4b3f214fe7e74e68efa279a1bb4be9258d06583a
Ryan Rodrigues <142359531+RyanRodrics@users.noreply.github.com>
2025-05-27 11:57:55 -0300

    feat: adicionar modal que cria threads de fan

--- src/controllers/CinemaController.java
+++ src/controllers/CinemaController.java
@@ -1,9 +1,13 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,25 +21,39 @@ public class CinemaController {
 	private Label labelCapacity;
 	@FXML
 	private Label labelFilmTime;
-	
+
 	private int capacity;
 	private int filmTime;
+	private Semaphore S;
+	private final AtomicInteger fanId = new AtomicInteger(1);
 
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 
+	private Stage newFanStage;
+	
+
 	@FXML
 	public void initialize() {
-		
+		//iniciar semaforos
 	}
-	
+
 	public void getInitialValues(int capacity, int filmTime) {
 		this.capacity = capacity;
 		this.filmTime = filmTime;
-		labelCapacity.setText(""+capacity);
-		labelFilmTime.setText(""+filmTime);
+		labelCapacity.setText("" + capacity);
+		labelFilmTime.setText("" + filmTime);
 	}
+	
+	public Semaphore getSemaphore() {
+		return this.S;
+	}
+	
+	public int getFanId() {
+		return this.fanId.getAndIncrement();
+	}
+	
 
 	public void switchToInitScene(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("/scenes/init.fxml"));
@@ -44,4 +62,34 @@ public class CinemaController {
 		stage.setScene(scene);
 		stage.show();
 	}
+
+	public void openNewFanWindow() {
+		if (newFanStage != null && newFanStage.isShowing()) {
+			newFanStage.toFront();
+			return;
+		}
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/scenes/new_fan.fxml"));
+			Parent root = loader.load();
+
+			NewFanController controller = loader.getController();
+			controller.setCinemaController(this);
+
+			newFanStage = new Stage();
+			newFanStage.setTitle("Adicionar Fan");
+			newFanStage.setScene(new Scene(root));
+			newFanStage.initModality(Modality.APPLICATION_MODAL); // bloqueia interação com a principal
+			newFanStage.setOnCloseRequest(e -> newFanStage = null); // se o usuário fechar manualmente
+			newFanStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void initFanAnimation(int id) {
+		System.out.println("Iniciada animação do fan "+id);
+	}
+
 }
--- src/threads/Fan.java
+++ src/threads/Fan.java
@@ -1,17 +1,20 @@
 package threads;
 
+import controllers.CinemaController;
 import java.util.concurrent.Semaphore;
 
 public class Fan extends Thread {
 
 	private int id;
-	private int lanchTime;
+	private int eatTime;
 	private Semaphore S;
+	private CinemaController cinema;
 
-	public Fan(int id, int lanchTime, Semaphore S) {
-		this.id = id;
-		this.lanchTime = lanchTime;
-		this.S = S;
+	public Fan(int eatTime, CinemaController cinemaController) {
+		this.id = cinemaController.getFanId();
+		this.S = cinemaController.getSemaphore();
+		this.eatTime = eatTime;
+		this.cinema = cinemaController;
 	}
 
 	public void eat() {
@@ -19,7 +22,14 @@ public class Fan extends Thread {
 	}
 
 	public void run() {
-
+		while (true) {
+			try {
+				Thread.sleep(1000);
+				cinema.initFanAnimation(this.id);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
