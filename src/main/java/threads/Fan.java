1de5c2a5840331d73f62baa163d456aa1b42cbfe
ari123rm <141646190+ari123rm@users.noreply.github.com>
2025-05-30 19:36:12 -0300

    arrumar o botao cancelar

---
+++ src/main/java/threads/Fan.java
@@ -0,0 +1,262 @@
+package main.java.threads;
+
+import java.util.concurrent.Semaphore;
+
+import main.java.controllers.CinemaController;
+import main.java.views.FanView;
+import main.java.views.PizzaView;
+import utils.Fila;
+
+public class Fan extends Thread {
+
+	private int id;
+	private int eatTime;
+
+	private CinemaController cinema;
+	private Semaphore cadeirasSemaphore;
+	private Semaphore poltronasSemaphore;
+	private Semaphore filaForaCinema;
+	private Semaphore filaAndou;
+	private Semaphore entrada ;
+	private Semaphore inicioFilme ;
+	private Semaphore cheios;
+	private Semaphore saida ;
+
+	public Fan(int eatTime, CinemaController cinemaController) {
+		this.id = cinemaController.getFanId();
+		this.eatTime = eatTime;
+		this.cinema = cinemaController;
+		this.cadeirasSemaphore = cinemaController.getCadeiras();
+		this.poltronasSemaphore = cinemaController.getPoltronas();
+		this.filaForaCinema = cinemaController.getFilaForaCinema();
+		this.entrada = cinemaController.getEntrada();
+		this.inicioFilme = cinemaController.getInicioFilme();
+		this.cheios = cinemaController.getCheios();
+		this.saida = cinemaController.getSaida();
+		this.filaAndou = cinemaController.getFilaAndou();
+		setDaemon(true);
+	}
+
+	public void eat(double x, double y) {
+		PizzaView pizza = cinema.createPizzaView(x, y, eatTime);
+		pizza.pizzaAnimation(eatTime);
+	}
+	public void eatVoid(){
+		long init = System.nanoTime();
+		while (init + eatTime * 1_000_000_000L > System.nanoTime()) {
+			int i = 1 + 1;
+		}
+	}
+	public void escolherPoltronaCinema() {
+		try {
+			poltronasSemaphore.acquire();
+			// Simula a escolha da cadeira
+			// Aqui você pode adicionar lógica para escolher uma cadeira específica
+			Fila poltronas = cinema.getPoltronasCinema();
+			int posicao = poltronas.positionEmpty();
+			int posicaoPessoa = poltronas.positionPerson(id);
+			if(posicaoPessoa != -1){
+				return;
+			}
+			if(posicao == -1) {
+				return;
+				
+			}
+			if(posicao < 0 ) {
+				return;
+			}
+			poltronas.addPerson(posicao,id);
+			//Thread.sleep(100); // Simula o tempo de escolha da cadeira
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+		} finally {
+			poltronasSemaphore.release();
+		}
+	}
+	public void sairPoltronaCinema(){
+		try {
+			poltronasSemaphore.acquire();
+			Fila poltronas = cinema.getPoltronasCinema();
+			int posicao = poltronas.positionPerson(id);
+			if(posicao != -1) {
+				poltronas.removePerson(posicao);
+			} else {
+			}
+			
+		} catch (Exception e) {
+			Thread.currentThread().interrupt();
+		} finally{
+			poltronasSemaphore.release();
+		}
+		
+	}
+	
+	public void escolherCadeiraPraca(){
+		try {
+			cadeirasSemaphore.acquire();
+			// Simula a escolha da cadeira
+			// Aqui você pode adicionar lógica para escolher uma cadeira específica
+			Fila cadeiras = cinema.getCadeirasPraca();
+			int posicao = cadeiras.positionEmpty();
+			int posicaoPessoa = cadeiras.positionPerson(id);
+			if(posicaoPessoa != -1){
+				
+				return;
+			}
+			if(posicao == -1) {
+				
+				cinema.addVoidPraca(id);
+				return;
+				
+			}
+			if(posicao < 0 ) {
+				
+				return;
+			}
+			cinema.removeVoidPraca(id);
+			cadeiras.addPerson(posicao,id);
+			
+			//Thread.sleep(100); // Simula o tempo de escolha da cadeira
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+		} finally {
+			cadeirasSemaphore.release();
+		}
+	}
+	public void sairCadeiraPraca(){
+		try {
+			cadeirasSemaphore.acquire();
+			Fila poltronas = cinema.getCadeirasPraca();
+			int posicao = poltronas.positionPerson(id);
+			if(posicao != -1) {
+				poltronas.removePerson(posicao);
+				
+			} else {
+				
+			}
+		} catch (Exception e) {
+			Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+		}finally {
+			cadeirasSemaphore.release();
+		}
+		
+
+	}
+	
+	public void escolherFilaCinema(){
+		try {
+			filaForaCinema.acquire();
+			// Simula a escolha da cadeira
+			// Aqui você pode adicionar lógica para escolher uma cadeira específica
+			Fila poltronas = cinema.getFilaCinema();
+			int posicao = poltronas.positionEmpty();
+			int posicaoPessoa = poltronas.positionPerson(id);
+			if(posicaoPessoa != -1){
+				return;
+			}
+			if(posicao == -1) {
+				cinema.addVoidCinema(id);
+				return;
+				
+			}
+			if(posicao < 0 ) {
+				return;
+			}
+			if(cinema.getVoidCinema().contains(id) && cinema.getVoidCinema().get(0)!= id){
+				return;
+			}
+			cinema.removeVoidCinema(id);
+			poltronas.addPerson(posicao,id);
+			//Thread.sleep(100); // 
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+		} finally {
+			filaForaCinema.release();
+		}
+	}
+	public void sairFilaForaCinema(){
+		try {
+			filaForaCinema.acquire();
+			Fila poltronas = cinema.getFilaCinema();
+			int posicao = poltronas.positionPerson(id);
+			if(posicao == 0) {
+				poltronas.removeFifo();
+			} else {
+				
+			}
+		} catch (Exception e) {
+			Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+		}finally {
+			filaForaCinema.release();
+		}
+	}
+	
+	public void run() {
+		FanView fan = cinema.createFanView(id,eatTime,this);
+		while (true) {
+			try {
+				fan.invisible();
+				fan.moveTo( 679, 60);
+				cinema.addConsoleText("Fan " + id + " entrou na fila");
+				while(true){
+					
+					escolherFilaCinema();
+					cinema.setLabelVoidQueue(cinema.getVoidCinema().size());
+					if(cinema.getFilaCinema().positionPerson(id) != -1){
+						fan.show();
+						fan.entryQueueAnimation(cinema.getFilaCinema().getPerson(id)[0], cinema.getFilaCinema().getPerson(id)[1]);
+					}
+					if(cinema.getFilaCinema().positionPerson(id) == 0)break;
+					
+					filaAndou.acquire();
+				}
+				
+				//Fã na fila do cinema
+				fan.entryAnimation(true);
+				entrada.acquire();
+				sairFilaForaCinema();
+				filaAndou.release(cinema.getFilaCinema().getTamanho() + cinema.getVoidCinema().size());
+				
+				escolherPoltronaCinema();
+				cinema.addConsoleText("Fan " + id + " entrou no cinema");
+				fan.goToCinemaChairAnimation( cinema.getPoltronasCinema().getPerson(id)[0], cinema.getPoltronasCinema().getPerson(id)[1], true);
+				
+				// Animção Fã entrando no cinema
+				
+
+				// Fã entra no cinema
+				cheios.release();
+
+				//Fã esperando filme
+				inicioFilme.acquire();
+
+				fan.watchFilmAnimation(cinema.getIsFilmRunningObject());
+				// Animação Fã saindo do cinema
+				sairPoltronaCinema();
+				fan.goToRefectoryAnimation(true);
+				saida.release();
+				escolherCadeiraPraca();
+				cinema.setLabelVoidRefectory(cinema.getVoidPraca().size());
+				cinema.addConsoleText("Fan " + id + " entrou na praca de alimentacao");
+				if(cinema.getCadeirasPraca().positionPerson(id) != -1){
+					fan.goToRefectoryChairAnimation(cinema.getCadeirasPraca().getPerson(id)[0], cinema.getCadeirasPraca().getPerson(id)[1], cinema.getCadeirasPraca().getPerson(id)[2]==1 , true);
+					eat(fan.getX(), fan.getY());
+					sairCadeiraPraca();
+					fan.goFromRefectoryToExitAnimation(true);
+				}else {
+					fan.goOutToEatAnimation(true);
+					cinema.setLabelVoidRefectory(cinema.getVoidPraca().size());
+					eatVoid();
+					cinema.removeVoidPraca(id);
+					cinema.setLabelVoidRefectory(cinema.getVoidPraca().size());
+					fan.goFromOutToExitAnimation(true);
+				}
+				
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/main/java/views/FanView.java
@@ -0,0 +1,322 @@
+package main.java.views;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javafx.application.Platform;
+import javafx.scene.layout.Pane;
+
+public class FanView {
+	private final int id;
+	private final ImageView imageView;
+	private double x, y;
+	private final Pane root;
+	private final List<Image> fanFrames = new ArrayList<>();
+	private int walkFrame = 1;
+	private int viewDirection = 3; // 1=up, 2=rigth, 3=down, 4=left
+
+	public FanView(int id, Pane root) {
+		this.id = id;
+		this.root = root;
+		this.x = 680;
+		this.y = 60;
+
+		for (int i = 1; i <= 92; i++) {
+			fanFrames.add(new Image("/assets/chaves_frames/chaves_frame_" + i + ".png"));
+		}
+
+		imageView = new ImageView(fanFrames.get(51));
+		imageView.setFitWidth(50);
+		imageView.setFitHeight(50);
+		imageView.setLayoutX(x);
+		imageView.setLayoutY(y);
+		// testando mudar a animação ao clicar
+		imageView.setOnMouseClicked(e -> {
+			// if (this.viewDirection == 4)
+			// this.changeDirection(1);
+			// else
+			// this.changeDirection(this.viewDirection + 1);
+			System.out.println("X: " + this.x + "Y: " + this.y);
+		});
+
+		// root.getChildren().add(imageView);
+		Platform.runLater(() -> root.getChildren().add(imageView));
+	}
+
+	public void entryQueueAnimation(double x, double y){
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (true) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;	
+				if(this.x> x)
+					walk();
+				else if (this.y < y){
+					this.viewDirection = 3;
+					walk();
+				}else 
+					break;
+			}
+		}
+	}
+
+	public void entryAnimation(boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 3;
+				if (this.y < 415)
+					walk();
+				else if (this.x > 640) {
+					this.viewDirection = 2;
+					walk();
+				} else
+					break;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+
+	public void goToCinemaChairAnimation(double chairX, double chairY, boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;
+				if (this.x > chairX)
+					walk();
+				else if (this.y > chairY) {
+					this.viewDirection = 1;
+					walk();
+				} else {
+					this.switchFrame(69);
+					this.walkFrame = 69;
+					break;
+				}
+			}
+		}
+	}
+
+	public void watchFilmAnimation(AtomicBoolean isFilmRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 100_000_000; // 42 ms
+		while (isFilmRunning.get()) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				switchFrame(walkFrame);
+				int walkFrame0 = 81;
+				if (this.walkFrame >= walkFrame0 && this.walkFrame < walkFrame0 + 2)
+					this.walkFrame++;
+				else
+					this.walkFrame = walkFrame0;
+				
+				
+			}
+		}
+	}
+
+	public void goToRefectoryAnimation(boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;
+				if (this.x > 222)
+					walk();
+				else if (this.y < 415) {
+					this.viewDirection = 3;
+					walk();
+				} else {
+					this.viewDirection = 2;
+					walk();
+					walk();
+					break;
+				}
+			}
+		}
+	}
+
+	public void goToRefectoryChairAnimation(double chairX, double chairY, boolean backwards, boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;
+				if (this.x > 194)
+					walk();
+				else if (this.y > chairY) {
+					this.viewDirection = 1;
+					walk();
+				} else if (this.x > chairX) {
+					this.viewDirection = 2;
+					walk();
+				} else {
+					if (backwards)
+						this.switchFrame(71);
+					else
+						this.switchFrame(77);
+					break;
+				}
+			}
+		}
+	}
+
+	public void goOutToEatAnimation(boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;
+				if (this.x > 46)
+					walk();
+				else if (this.y < 474) {
+					this.viewDirection = 3;
+					walk();
+				} else
+					break;
+			}
+		}
+	}
+
+	public void goFromRefectoryToExitAnimation(boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 2;
+				if (this.x > 26)
+					walk();
+				else if (this.y > 250) {
+					this.viewDirection = 1;
+					walk();
+				} else
+					break;
+			}
+		}
+	}
+
+	public void goFromOutToExitAnimation(boolean isRunning) {
+		long lastUpdate = System.nanoTime();
+		final long frameDuration = 65_000_000; // 42 ms
+		while (isRunning) {
+			long now = System.nanoTime();
+			if (now - lastUpdate >= frameDuration) {
+				lastUpdate = now;
+				this.viewDirection = 1;
+				if (this.y < 326)
+					walk();
+				else {
+					this.goFromRefectoryToExitAnimation(isRunning);
+					break;
+				}
+			}
+		}
+	}
+
+	public void walk() {
+		switchFrame(walkFrame);
+		int walkFrame0 = (this.viewDirection - 1) * 9 + 33;
+		if (this.walkFrame >= walkFrame0 && this.walkFrame < walkFrame0 + 8)
+			this.walkFrame++;
+		else
+			this.walkFrame = walkFrame0;
+
+		if (viewDirection == 1)
+			moveTo(x, y - 2);
+		if (viewDirection == 2)
+			moveTo(x - 2, y);
+		if (viewDirection == 3)
+			moveTo(x, y + 2);
+		if (viewDirection == 4)
+			moveTo(x + 2, y);
+	}
+
+	public void run() {
+		switchFrame(walkFrame);
+		int walkFrame0 = (this.viewDirection - 1) * 8 + 1;
+		if (this.walkFrame >= walkFrame0 && this.walkFrame < walkFrame0 + 7)
+			this.walkFrame++;
+		else
+			this.walkFrame = walkFrame0;
+
+		if (viewDirection == 1)
+			moveTo(x, y - 2);
+		if (viewDirection == 2)
+			moveTo(x - 2, y);
+		if (viewDirection == 3)
+			moveTo(x, y + 2);
+		if (viewDirection == 4)
+			moveTo(x + 2, y);
+	}
+
+	public void changeDirection(int newDirection) {
+		if (newDirection > 0 && newDirection < 5) {
+			this.viewDirection = newDirection;
+		}
+	}
+
+	public void switchFrame(int newFrameId) {
+		Platform.runLater(() -> {
+			if (newFrameId >= 1 && newFrameId <= fanFrames.size()) {
+				this.imageView.setImage(fanFrames.get(newFrameId - 1));
+			}
+		});
+	}
+
+	public void moveTo(double newX, double newY) {
+		Platform.runLater(() -> {
+			this.x = newX;
+			this.y = newY;
+			imageView.setLayoutX(x);
+			imageView.setLayoutY(y);
+		});
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
+	public ImageView getImageView () {
+		return this.imageView;
+	}
+
+	public void remove() {
+		Platform.runLater(() -> root.getChildren().remove(imageView));
+	}
+	public void invisible(){
+		imageView.setOpacity(0);
+	}
+	public void show(){
+		imageView.setOpacity(100);
+	}
+}
